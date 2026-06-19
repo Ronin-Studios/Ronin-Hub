2e957a88ec40d49d6cdcd6677e33cc9638a3d7fc
hina <creepbrute.rbx@gmail.com>
2026-06-19 21:40:53 +0200

    Add supported games configuration

--- supported-games.lua
+++ supported-games.lua
@@ -1,1 +1,18 @@
+return {
+    Discord = "https://discord.gg/uuaFAm6AdE",
 
+    Maintenance = false,
+    MaintenanceMessage = "Ronin Hub is undergoing maintenance. Check Discord for updates.",
+
+    Games = {
+        {
+            Name   = "Grow a Garden 2",
+            Ids    = { 10200395747, 97598239454123 }, 
+            Script = "https://raw.githubusercontent.com/Ronin-Studios/Ronin-Hub/refs/heads/main/games/Grow-A-Garden-2.lua",
+            Disabled = false,
+            DisabledMessage = "Grow a Garden 2 support is being updated, check back soon.",
+        },
+
+    
+    },
+}
